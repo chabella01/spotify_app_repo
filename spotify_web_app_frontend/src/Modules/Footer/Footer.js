9bf02ce2c86b0ac55e388e044931e0103b66d9c0
User <User@MacBook-Air.local>
2023-12-05 15:18:02 -0800

    added footer

---
+++ spotify_web_app_frontend/src/Modules/Footer/Footer.js
@@ -0,0 +1,14 @@
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+function Footer(){
+    return(
+        
+            <p class="container-fluid bg-dark text-light pt-5 pb-5 text-center">
+            <p>&copy;Spotify Connections 2023</p>
+    
+            </p>
+        
+    )
+}
+export default Footer
--- spotify_web_app_frontend/src/Modules/Layout/Layout.js
+++ spotify_web_app_frontend/src/Modules/Layout/Layout.js
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header.js";
+import Footer from "../Footer/Footer.js"
 
 const Layout = () => {
     return (
         <>
             <Header />
             <Outlet />
+            <Footer />
         </>
     );
 };
