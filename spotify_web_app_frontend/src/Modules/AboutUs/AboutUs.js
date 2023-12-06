9996f9ace6ef9fde67806e65e84fd35608edfd6b
steven102814 <steven_sanchez17@yahoo.com>
2023-12-05 20:06:41 -0800

    Default cards with default pics

--- spotify_web_app_frontend/src/Modules/AboutUs/AboutUs.js
+++ spotify_web_app_frontend/src/Modules/AboutUs/AboutUs.js
@@ -2,63 +2,64 @@ import {React, useState} from 'react'
 import './AboutUs.css'
 import {useNavigate} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import pfp from "../../Assets/Images/pfp_demo.png"
 
 function AboutUs() {
     return (
 
         <div class="card-group">
             <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <img class="card-img-top" src={pfp} alt="Card image cap"/>
                 <div class="card-body">
                 <h5 class="card-title">Jacob</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text">Computer Engineer and Musician Hobbyist.</p>
                 </div>
                 <div class="card-footer">
                 <small class="text-muted">Last updated 3 mins ago</small>
                 </div>
             </div>
             <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <img class="card-img-top" src={pfp} alt="Card image cap"/>
                 <div class="card-body">
                 <h5 class="card-title">Christian</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text">Computer Engineer.</p>
                 </div>
                 <div class="card-footer">
                 <small class="text-muted">Last updated 3 mins ago</small>
                 </div>
             </div>
             <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <img class="card-img-top" src={pfp} alt="Card image cap"/>
                 <div class="card-body">
                 <h5 class="card-title">Justin</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text">Computer Engineer.</p>
                 </div>
                 <div class="card-footer">
                 <small class="text-muted">Last updated 3 mins ago</small>
                 </div>
             </div>
             <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <img class="card-img-top" src={pfp} alt="Card image cap"/>
                 <div class="card-body">
                 <h5 class="card-title">David</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text">Computer Engineer.</p>
                 </div>
                 <div class="card-footer">
                 <small class="text-muted">Last updated 3 mins ago</small>
                 </div>
             </div>
             <div class="card">
-                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <img class="card-img-top" src={pfp} alt="Card image cap"/>
                 <div class="card-body">
                 <h5 class="card-title">Steven</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text">Computer Engineer and Car Enthusiast.</p>
                 </div>
                 <div class="card-footer">
                 <small class="text-muted">Last updated 3 mins ago</small>
                 </div>
             </div>
             </div>
-        
+
         );
     }
 
