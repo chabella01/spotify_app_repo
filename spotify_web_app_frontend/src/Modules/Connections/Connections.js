362e43b0138a82201c0afcdd707f856d3d9ef38e
Justin Taing <justinxuetaing@gmail.com>
2023-12-06 00:02:24 -0800

    4 number limit on the sessions page

--- spotify_web_app_frontend/src/Modules/Connections/Connections.js
+++ spotify_web_app_frontend/src/Modules/Connections/Connections.js
@@ -1,6 +1,7 @@
 import {React, useState} from 'react'
 import './Connections.css'
 import {useNavigate} from "react-router-dom";
+import pfp_demo from '../../Assets/Images/pfp_demo.png'
 
 function Connections() {
 
@@ -24,20 +25,37 @@ function Connections() {
 
     const handleChangeSessionId = (e) => {
         setSessionId(e.target.value)
+        let value = e.target.value;
+        // Remove non-numeric characters
+        value = value.replace(/\D/g, '');
+    
+        // Limit input to 4 digits
+        if (value.length > 4) {
+          value = value.slice(0, 4);
+        }
+    
+        setSessionId(value);
     }
 
     const getProfileData = () => {
-        const profile = localStorage.getItem('profile')
+        const profile = sessionStorage.getItem('profile')
 
-        const {display_name} = JSON.parse(profile)
+        const {display_name, images} = JSON.parse(profile)
 
         if(profile) {
             return (
-                <div>
-                    Welcome {display_name}
+                <div class="card text-bg-dark">
+                    <img className={"rcard-img-top"} src={images.length === 0 ? pfp_demo : images[0].url}/>
+                    <h2 className={"card-title"}>
+                        Welcome {display_name}
+
+                    </h2>
+
                 </div>
             )
 
+        } else {
+            return <div>no profule</div>
         }
     }
 
@@ -46,23 +64,23 @@ function Connections() {
             {getProfileData()}
                 <title>Spotify Page to create session button</title>
                 <link rel="stylesheet" href="connection_page_styles.css"/>
-            <div className={'form-container'}>
+            <form class="form-floating">
                 <label htmlFor="numericInput">Enter 4-digit Session ID:</label>
                 <input
-                    type="number"
+                    type="text" 
                     id="numericInput"
                     name="numericInput"
-                    min="0"
-                    max="9999"
-                    maxLength="4"
+                    className="form-control"
+                    value={sessionId}
                     onChange={handleChangeSessionId}
-                />
+                    />
                 <button className={'session-button'}
-                    onClick={handleClickCreateSession}
+                    onClick={handleClickJoinSession}
                 >
                     Join Existing Session</button>
-                <button className={"session-button"}>Or Create A New Session</button>
-            </div>
+                <button className={"session-button"}
+                    onClick={handleClickCreateSession}>Or Create A New Session</button>
+            </form>
         </div>
 
     )
