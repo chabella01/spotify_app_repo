80d41b72b9ca467340400b0430276397c2484f9a
Justin Taing <justinxuetaing@gmail.com>
2023-12-06 10:43:40 -0800

    This is the new git pull origin

---
+++ spotify_web_app_frontend/src/Modules/Footer/Footer.js
@@ -0,0 +1,17 @@
+
+import 'bootstrap/dist/css/bootstrap.css'
+import './Footer.css'
+
+function Footer(){
+    return(
+        <div class="footer-container">
+            <div class="footer-text">
+            <p class="container-fluid bg-dark text-light pt-5 pb-5 text-center">
+                &copy;Spotify Connections 2023
+            </p>
+            </div>
+        </div>
+        
+    )
+}
+export default Footer
