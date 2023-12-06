3b4edaaaf5087b1dc620d7c191c0c09f7ed826b3
Steven Sanchez <stevensanchez@Stevens-MacBook-Pro.local>
2023-12-05 17:28:44 -0800

    Added cards for each team members

--- spotify_web_app_frontend/src/Modules/AboutUs/AboutUs.js
+++ spotify_web_app_frontend/src/Modules/AboutUs/AboutUs.js
@@ -9,14 +9,59 @@ function AboutUs() {
         // About Us
         // </h1>
 
-        <div className="about-us-card card">
-            <img src="your_image_path.jpg" className="card-img-top" alt="About Us" />
-            <div className="card-body">
-                <h5 className="card-title">About Us</h5>
-                <p className="card-text">Information about your site or service.</p>
-                {/* Other content */}
+        <div class="card-group">
+            <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <div class="card-body">
+                <h5 class="card-title">Jacob</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
             </div>
-        </div>
+            <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <div class="card-body">
+                <h5 class="card-title">Christian</h5>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <div class="card-body">
+                <h5 class="card-title">Justin</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                </div>
+                <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <div class="card-body">
+                <h5 class="card-title">David</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                </div>
+                <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <div class="card-body">
+                <h5 class="card-title">Steven</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                </div>
+                <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            </div>
+        
         );
     }
 
